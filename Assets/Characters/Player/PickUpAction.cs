b96347a2fc2e7d418faf551d19b9d73f28de4684
Hakebbun <kevin.hugh93@gmail.com>
2023-11-21 20:38:40 -0400

    Revert "Grid system"

--- Assets/Characters/Player/PickUpAction.cs
+++ Assets/Characters/Player/PickUpAction.cs
@@ -7,40 +7,15 @@ public class PickUpAction : MonoBehaviour
 
     public LayerMask pickUpMask;
     public Transform holdSpot;
-    public Transform towerHoldSpot;
     public GameObject itemHolding = null;
     public GameObject pickupSpot;
-    public float pickupObjectScale;
-    private Vector3 objectOriginalScale;
-
-    public GameObject placementCursor;
-    public GameObject placementError;
-    public float cursorDistanceFromPlayer;
-    private Vector2 cursorPosition;
-    private LayerMask gridSpaceMask;
 
     private PlayerController playerController;
 
     void Start() {
         pickUpMask = LayerMask.GetMask("Moveable", "Upgrade");
-        gridSpaceMask = LayerMask.GetMask("GridSpace");
         playerController = GetComponent<PlayerController>();
         LevelController.OnPhaseChange += OnPhaseChange;
-        placementCursor = Instantiate(placementCursor);
-        placementCursor.SetActive(false);
-        placementError = Instantiate(placementError);
-        placementError.SetActive(false);
-    }
-
-    private void Update()
-    {
-        if (itemHolding)
-        {
-            cursorPosition = this.transform.position;
-            cursorPosition += (playerController.PrevMoveDirection * GridHelper.gridSize * cursorDistanceFromPlayer);
-            cursorPosition = GridHelper.ClosestGridPoint(cursorPosition);
-            placementCursor.transform.position = cursorPosition;
-        }
     }
 
     public void pickUpObject() {
@@ -59,16 +34,10 @@ public class PickUpAction : MonoBehaviour
 
     public void dropItem() {
         if (itemHolding != null) {
-            // Check if anything will obstruct the 'drop'
-            if (CheckForObject(cursorPosition))
-                return;
-
             itemHolding.transform.parent = null;
-            if (itemHolding.GetComponent<Rigidbody2D>()) {
-                    itemHolding.GetComponent<Rigidbody2D>().simulated = true;
-                }
-            itemHolding.transform.position = GridHelper.ClosestGridPoint(cursorPosition);
-            placementCursor.SetActive(false);
+                    if (itemHolding.GetComponent<Rigidbody2D>()) {
+                            itemHolding.GetComponent<Rigidbody2D>().simulated = true;
+                        }
             ResetScaleOnDrop(itemHolding);
             itemHolding = null;
         }
@@ -97,15 +66,12 @@ public class PickUpAction : MonoBehaviour
             
             if (toPickUp) {
                 itemHolding = toPickUp.gameObject;
-                objectOriginalScale = toPickUp.gameObject.transform.localScale;
-                itemHolding.transform.localScale = itemHolding.transform.localScale * pickupObjectScale;
-                itemHolding.transform.position = towerHoldSpot.position;
+                itemHolding.transform.position = holdSpot.position;
                 itemHolding.transform.parent = transform;
 
-            if (itemHolding.GetComponent<Rigidbody2D>()) {
+                if (itemHolding.GetComponent<Rigidbody2D>()) {
                     itemHolding.GetComponent<Rigidbody2D>().simulated = false;
-                    placementCursor.SetActive(true);
-                return true;
+                    return true;
                 }
             }
             return false;
@@ -157,26 +123,8 @@ public class PickUpAction : MonoBehaviour
     }
 
     private void ResetScaleOnDrop(GameObject objectToDrop) {
-        //Vector3 currentScale = objectToDrop.transform.localScale;
-        //currentScale.x = Mathf.Abs(currentScale.x);
-        //objectToDrop.transform.localScale = currentScale;
-        objectToDrop.transform.localScale = objectOriginalScale;
-    }
-
-    private bool CheckForObject(Vector2 checkPosition)
-    {
-        Vector2 checkBoxSize = new Vector2(GridHelper.gridSize * 2, GridHelper.gridSize * 2);
-        RaycastHit2D hit = Physics2D.BoxCast(checkPosition, checkBoxSize, 0f, Vector2.up, 0f, gridSpaceMask);
-
-        if (hit.collider != null)
-        {
-            Debug.Log(hit.collider.gameObject.layer);
-            placementError.SetActive(false);
-            placementError.transform.position = hit.collider.transform.position;
-            placementError.SetActive(true);
-            return true;
-        }
-
-        return false;
+        Vector3 currentScale = objectToDrop.transform.localScale;
+        currentScale.x = Mathf.Abs(currentScale.x);
+        objectToDrop.transform.localScale = currentScale;
     }
 }
